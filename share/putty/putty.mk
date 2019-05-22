f93930e33201eee71ef85c049e84b3e94b04441c
Dan Page <dan@phoo.org>
2019-05-22 14:43:21 +0100

    Added emulator, and updated build system to include support for Putty-based interaction.

---
+++ share/putty/putty.mk
@@ -0,0 +1,17 @@
+# Copyright (C) 2017 Daniel Page <dan@phoo.org>
+#
+# Use of this source code is restricted per the CC BY-SA license, a copy of
+# which can be found via http://creativecommons.org (and should be included 
+# as LICENSE.txt within the associated archive or repository).
+
+${HOME}/.putty/sessions/scale-board    : ${BSP}/share/putty.scale-board
+	@cp ${BSP}/share/putty.scale-board    ${@}
+
+${HOME}/.putty/sessions/scale-emulator : ${BSP}/share/putty.scale-emulator
+	@cp ${BSP}/share/putty.scale-emulator ${@}
+
+putty-board    : ${HOME}/.putty/sessions/scale-board
+	@putty -load $(notdir ${<})
+
+putty-emulator : ${HOME}/.putty/sessions/scale-emulator
+	@putty -load $(notdir ${<})
